8c4d454533eeaad75003666cd9d17142a05ba840
Nicolas Perussi <nicolasperussi12@hotmail.com>
2024-01-14 14:08:34 -0300

    init project

---
+++ src/main/java/com/nicolasperussi/scriptburger/ScriptburgerApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasperussi.scriptburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptburgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolasperussi/scriptburger/ScriptburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasperussi.scriptburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
