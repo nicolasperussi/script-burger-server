81e648eccd5f8b9963e575b57fa840e318b22320
Nicolas Perussi <nicolasperussi12@hotmail.com>
2024-01-16 15:04:24 -0300

    feat(seed): creation of admin user on start for debuggin purposes

--- src/main/java/com/nicolasperussi/scriptburger/config/Seeding.java
+++ src/main/java/com/nicolasperussi/scriptburger/config/Seeding.java
@@ -2,13 +2,20 @@ package com.nicolasperussi.scriptburger.config;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nicolasperussi.scriptburger.domain.Address;
 import com.nicolasperussi.scriptburger.domain.Product;
+import com.nicolasperussi.scriptburger.domain.User;
+import com.nicolasperussi.scriptburger.domain.enums.UserRole;
+import com.nicolasperussi.scriptburger.repositories.UserRepository;
 import com.nicolasperussi.scriptburger.services.ProductService;
 import com.nicolasperussi.scriptburger.utils.Slugify;
 
@@ -17,8 +24,16 @@ public class Seeding implements CommandLineRunner {
   @Autowired
   ProductService productService;
 
+  @Autowired
+  private UserRepository userRepository;
+
   @Override
   public void run(String... args) throws Exception {
+    populateProducts();
+    createAdminUser();
+  }
+
+  private void populateProducts() {
     try {
       ObjectMapper objectMapper = new ObjectMapper();
       Product[] products = objectMapper.readValue(new File("public/products.json"), Product[].class);
@@ -32,6 +47,17 @@ public class Seeding implements CommandLineRunner {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private void createAdminUser() {
+    String encryptedPassword = new BCryptPasswordEncoder().encode("admin");
+    Address defaultAddress = new Address("01001-000", "Praça da Sé", "0");
+    List<Address> addresses = new ArrayList<Address>();
+    addresses.add(defaultAddress);
+
+    User newUser = new User("Administrator", "admin@admin.com", "999999999", encryptedPassword, addresses);
+    newUser.setRole(UserRole.ADMIN);
+    this.userRepository.save(newUser);
   }
 
 }
